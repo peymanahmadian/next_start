405b36b29861c1b1db3ec9351730ee2675afc404
peyman ahmadian <ahmadian.peyman@setare.ir>
2026-07-10 19:01:54 +0330

    setup slug and 404

---
+++ src/app/blog/[[...slug]]/page.tsx
@@ -0,0 +1,9 @@
+interface IPage {
+    params: Promise<{ slug: string[] }>
+}
+export default async function Page(props:IPage){ 
+
+	const { slug } = await props.params;
+
+	return <div>Slug: {slug && slug.join('/')}</div>
+}
--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,11 +1,12 @@
 import ShowID from "@/components/show-id/ShowID";
-
+import {notFound} from "next/navigation";
 type PageProps = {
 	params:Promise<{id:number}>;
 };
 
 export default async function ProductPage(props:{params:Promise<{id:number}>}) {
 	const { id } = await props.params;
+	if(id>5) return notFound();
 	return <>
 		<ShowID/>
 
